69d8abb6c0eabe344fa561b3dde53efb5bb6ef71
David <david.ngu3@gmail.com>
2021-12-02 16:37:36 +1100

    classes

--- book.js
+++ book.js
@@ -1,81 +1,94 @@
-let myLibrary = [];
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
-
-Book.prototype.info = function() {
-    let readString = this.read ? "read" : "not read yet";
+    info() {
+        let readString = this.read ? "read" : "not read yet";
         return `${title} by ${author}, ${pages} pages, ${readString}`;
+    }
+
+    changeReadStatus() {
+        this.read = !this.read; 
+    }
 }
 
-Book.prototype.changeReadStatus = function() {
-    this.read = !this.read; // invert read status
+class Library {
+    constructor() {
+        this.books = [];
+    }
+
+    addBookToLibrary(book) {
+        this.books.push(book);
+    }
+
+    addSampleBooks() {
+        let theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, true);
+        let theShining = new Book("The Shining", "S.C Mallien", 323, false);
+        let endersGame = new Book("Ender's Game", "S. B. Ballik", 333, true);
+        let minecraft = new Book("Minecraft", "Jesus Christ", 211, false);
+
+        this.addBookToLibrary(theHobbit);
+        this.addBookToLibrary(theShining);
+        this.addBookToLibrary(endersGame);
+        this.addBookToLibrary(minecraft);
+    }
 }
-
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-}
-
-function displayBooks() {
-    let cardContainer = document.getElementById("card-container");
-    cardContainer.innerHTML = ""; // clear cards
-
-    myLibrary.forEach((book, idx) => {
-        let bookTitle = document.createElement("h2");
-        bookTitle.innerText = book.title;
-        let bookAuthor = document.createElement("h2");
-        bookAuthor.innerText = book.author;
-        let bookPages = document.createElement("p");
-        bookPages.innerText = `${book.pages} Pages`;
-        let bookRead = document.createElement("p");
-        bookRead.innerText = book.read ? "Read" : "Not Read";
-
-        let readBtn = document.createElement("button");
-        readBtn.innerHTML = "Read/Unread";
-        readBtn.className = "btn btn-secondary";
-        readBtn.addEventListener('click', function() {
-            book.changeReadStatus();
-            displayBooks();
-        });
-
-        let deleteBtn = document.createElement("button");
-        deleteBtn.innerHTML = "X";
-        deleteBtn.className = "btn btn-danger";
-        deleteBtn.addEventListener('click', function() {
-            myLibrary.splice(idx, 1);
-            displayBooks();
-        });
-
-        let bookCard = document.createElement("div");
-        bookCard.appendChild(bookTitle);
-        bookCard.appendChild(bookAuthor);
-        bookCard.appendChild(bookPages);
-        bookCard.appendChild(bookRead);
-        bookCard.appendChild(readBtn);
-        bookCard.appendChild(deleteBtn);
-        bookCard.className = "card";
-
-        cardContainer.appendChild(bookCard);
-    })
-}
-
-let theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, true);
-let theShining = new Book("The Shining", "S.C Mallien", 323, false);
-let endersGame = new Book("Ender's Game", "S. B. Ballik", 333, true);
-let minecraft = new Book("Minecraft", "Jesus Christ", 211, false);
-
-addBookToLibrary(theHobbit);
-addBookToLibrary(theShining);
-addBookToLibrary(endersGame);
-addBookToLibrary(minecraft);
 
 document.addEventListener('DOMContentLoaded', function() {
+    let lib = new Library();
+    lib.addSampleBooks();
+
+    // display books to screen
+    let cardContainer = document.getElementById("card-container");
     displayBooks();
 
+    function displayBooks() {
+        cardContainer.innerHTML = ""; 
+
+        lib.books.forEach((book, idx) => {
+            let bookTitle = document.createElement("h2");
+            bookTitle.innerText = book.title;
+            let bookAuthor = document.createElement("h2");
+            bookAuthor.innerText = book.author;
+            let bookPages = document.createElement("p");
+            bookPages.innerText = `${book.pages} Pages`;
+            let bookRead = document.createElement("p");
+            bookRead.innerText = book.read ? "Read" : "Not Read";
+    
+            let readBtn = document.createElement("button");
+            readBtn.innerHTML = "Read/Unread";
+            readBtn.className = "btn btn-secondary";
+            readBtn.addEventListener('click', function() {
+                book.changeReadStatus();
+                displayBooks();
+            });
+    
+            let deleteBtn = document.createElement("button");
+            deleteBtn.innerHTML = "X";
+            deleteBtn.className = "btn btn-danger";
+            deleteBtn.addEventListener('click', function() {
+                lib.books.splice(idx, 1);
+                displayBooks();
+            });
+    
+            let bookCard = document.createElement("div");
+            bookCard.appendChild(bookTitle);
+            bookCard.appendChild(bookAuthor);
+            bookCard.appendChild(bookPages);
+            bookCard.appendChild(bookRead);
+            bookCard.appendChild(readBtn);
+            bookCard.appendChild(deleteBtn);
+            bookCard.className = "card";
+        
+            cardContainer.appendChild(bookCard);
+        });
+    }
+    
+    // new book form
     let addBookBtn = document.getElementById("addBookBtn");
     addBookBtn.addEventListener('click', function() {
         let newBookTitle = document.getElementById("formTitle").value;
@@ -87,8 +100,7 @@ document.addEventListener('DOMContentLoaded', function() {
         let readBool = newBookRead.checked ? true : newBookNotRead ? false : null;
 
         let newBook = new Book(newBookTitle, newBookAuthor, newBookPages, readBool);
-        addBookToLibrary(newBook);
+        lib.addBookToLibrary(newBook);
         displayBooks();
     });
-
 });
